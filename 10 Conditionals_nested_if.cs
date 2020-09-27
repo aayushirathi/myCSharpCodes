f3c4486c66a92d044ce35c748d5bb3c29f0505e6
aayushirathi <71968675+aayushirathi@users.noreply.github.com>
2020-09-27 17:17:36 +0530

    Add files via upload

---
+++ 10 Conditionals_nested_if.cs
@@ -0,0 +1,13 @@
+using System; 
+class GFG { 
+    public static void Main(String[] args) 
+    { 
+        int i = 10; 
+        if (i == 10) { 
+            if (i < 12) 
+                Console.WriteLine("i is smaller than 12 too"); 
+            else
+                Console.WriteLine("i is greater than 15"); 
+        } 
+    } 
+} 
