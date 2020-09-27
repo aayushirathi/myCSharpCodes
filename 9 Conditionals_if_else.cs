9a15baa6a4a8efa5fbbfc910429f81311fdfcdb2
aayushirathi <71968675+aayushirathi@users.noreply.github.com>
2020-09-27 17:16:28 +0530

    Add files via upload

---
+++ 9 Conditionals_if_else.cs
@@ -0,0 +1,15 @@
+using System; 
+  
+public class GFG { 
+  
+    public static void Main(string[] args) 
+    { 
+        string name = "Geek"; 
+        if (name == "Geeks") { 
+            Console.WriteLine("GeeksForGeeks"); 
+        } 
+        else { 
+            Console.WriteLine("Geeks"); 
+        } 
+    } 
+} 
