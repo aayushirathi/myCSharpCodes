fce8efcf7a319c7e78d7fa5286258b89cf2c35cc
aayushirathi <71968675+aayushirathi@users.noreply.github.com>
2020-09-27 17:09:35 +0530

    Add files via upload

---
+++ 5 Display_Literal_Values.cs
@@ -0,0 +1,12 @@
+using System; 
+class Geeks { 
+    public static void Main(String []args) 
+    { 
+    int a = 101;
+    int b = 0145; 
+    int c = 0xFace; 
+    Console.WriteLine(a); 
+    Console.WriteLine(b); 
+    Console.WriteLine(c); 
+    } 
+} 
