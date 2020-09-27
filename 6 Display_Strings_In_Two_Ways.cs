39029262afef4f02bf031de217f607d845749fcb
aayushirathi <71968675+aayushirathi@users.noreply.github.com>
2020-09-27 17:12:27 +0530

    Add files via upload

---
+++ 6 Display_Strings_In_Two_Ways.cs
@@ -0,0 +1,12 @@
+using System; 
+class Geeks { 
+    public static void Main(String []args) 
+    { 
+          
+        String s1 = "Hello Geeks!"; 
+        String s2 = @"Hello Geeks!"; 
+        Console.WriteLine(s1); 
+        Console.WriteLine(s2); 
+          
+    } 
+} 
