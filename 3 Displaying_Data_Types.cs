1a174f33a5c0ae1e7bb295d2b8b8a31455bab154
aayushirathi <71968675+aayushirathi@users.noreply.github.com>
2020-09-27 17:07:26 +0530

    Add files via upload

---
+++ 3 Displaying_Data_Types.cs
@@ -0,0 +1,11 @@
+using System; 
+namespace ValueTypeTest { 
+class GeeksforGeeks { 
+   static void Main() { 
+     char a = 'G'; 
+     int i = 89; 
+     Console.WriteLine("char: " + a); 
+     Console.WriteLine("integer: " + i); 
+    } 
+} 
+} 
