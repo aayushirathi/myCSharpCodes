036be2e8ebde06a4f2c94066522f0a7367115ff0
aayushirathi <71968675+aayushirathi@users.noreply.github.com>
2020-09-28 22:06:00 +0530

    Add files via upload

---
+++ 59 ValueTuple.cs
@@ -0,0 +1,11 @@
+Link- https://onlinegdb.com/Byx0rXXPrv
+
+using System;
+public class GFG {
+  static public void Main(){
+    var author = (20,"Aayushi","ASP.NET");
+    Console.WriteLine("Age: "+author.Item1);
+    Console.WriteLine("Name: "+author.Item2);
+    Console.WriteLine("Language: "+author.Item3);   
+  }
+}
---
+++ 60 ValueTuple_Named_Members.cs
@@ -0,0 +1,14 @@
+Link- https://onlinegdb.com/BJe9QHPrw
+
+
+using System;
+public class GFG{
+  static public void Main(){
+    var library = (Book_id: 1234, Author_Name:"ABC", Book_Name:"BCDE");
+    Console.WriteLine("Author_Name: "+ library.Author_Name);
+    Console.WriteLine("Book_id: "+ library.Book_id);  
+    Console.WriteLine("Book_Name: "+ library.Book_Name); 
+  }
+
+  
+}
