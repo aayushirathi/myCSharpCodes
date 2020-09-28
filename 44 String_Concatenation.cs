cc80a27ba49927a9d923b438f74e4882ffc2e379
aayushirathi <71968675+aayushirathi@users.noreply.github.com>
2020-09-28 21:43:08 +0530

    Add files via upload

---
+++ 44 String_Concatenation.cs
@@ -0,0 +1,11 @@
+using System;
+class Geeks {
+  public static void Main(String[] args) {
+    string s1 = "I";
+    string s2 = " am";
+    string s3 = " Aayushi";
+    string s4 = " Rathi";
+    string str = s1+s2+s3+s4+". How are you doing.";
+    Console.WriteLine(str);
+  }
+}
---
+++ 45 Creating_String_using char array.cs
@@ -0,0 +1,10 @@
+using System;
+class Geeks {
+  public static void Main(String[] args) {
+    char[] chars = {'A', 'A', 'Y', 'U', 'S', 'H', 'I'};
+    String str1 = new String(chars);
+    Console.WriteLine(str1);
+    string str2 = new string('G', 10);
+    Console.WriteLine(str2);
+  }
+}
